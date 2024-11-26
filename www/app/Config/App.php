709395283ab0b08ef40fd23f842f75aec32b92b1
Dennis joel <dennismwagiru@gmail.com>
2024-11-26 09:48:29 +0300

    Update error reporting and base URL in config files
    Set error_reporting to E_ALL for better error visibility in development.php. Also, modify the baseURL to 'http://localhost/' in App.php for a more generic setup.

--- www/app/Config/App.php
+++ www/app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost/hrsale/';
+	public $baseURL = 'http://localhost/';
 
 	/*
 	|--------------------------------------------------------------------------
